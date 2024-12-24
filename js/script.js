969489b74c2afddbccea93dc66c7502454ac1bdc
Ridwan Romly <62383518+ridwanromly@users.noreply.github.com>
2024-12-24 11:46:03 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function onMenuClick() {
+    var navbar = document.getElementById("navigation-bar");
+    var responsive_class_name = "responsive";
+    var $body = document.querySelector("body");
+    
+    navbar.classList.toggle(responsive_class_name);
+
+}
